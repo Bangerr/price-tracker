d583575cbc73661f2bfe6fdac5cf306d4e39a24a
Bangerr <devilism09@gmail.com>
2024-10-07 18:56:49 +0200

    test

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -6,52 +6,52 @@ import { extractCurrency, extractDiscount, extractPrice } from "../utils";
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
 
-  // BrightData proxy configuration
-  const username = String(process.env.BRIGHT_DATA_USERNAME);
-  const password = String(process.env.BRIGHT_DATA_PASSWORD);
-  const port = 22225;
-  const session_id = (1000000 * Math.random()) | 0;
-  const options = {
-    auth: {
-      username: `${username}-session-${session_id}`,
-      password,
-    },
-    host: "brd.superproxy.io",
-    port,
-    rejectUnauthorized: false,
-  };
+  // // BrightData proxy configuration
+  // const username = String(process.env.BRIGHT_DATA_USERNAME);
+  // const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  // const port = 22225;
+  // const session_id = (1000000 * Math.random()) | 0;
+  // const options = {
+  //   auth: {
+  //     username: `${username}-session-${session_id}`,
+  //     password,
+  //   },
+  //   host: "brd.superproxy.io",
+  //   port,
+  //   rejectUnauthorized: false,
+  // };
 
   try {
     // Fetch product page
-    const response = await axios.get(url, options);
-    const $ = cheerio.load(response.data);
+    // const response = await axios.get(url, options);
+    // const $ = cheerio.load(response.data);
 
-    // Extract needed values from response data
-    const title = $("#productTitle").text().trim();
+    // // Extract needed values from response data
+    // const title = $("#productTitle").text().trim();
 
-    //const currentPrice = $("span.a-price-whole:first").text();
+    // //const currentPrice = $("span.a-price-whole:first").text();
 
-    const currentPrice = extractPrice(
-      $(".priceToPay span.a-price-whole:first"),
-      $(".a.size.base.a-color-price:first"),
-      $(".a-button-selected .a-color-base:first"),
-      $("#priceblock_dealprice_row:first")
-    );
+    // const currentPrice = extractPrice(
+    //   $(".priceToPay span.a-price-whole:first"),
+    //   $(".a.size.base.a-color-price:first"),
+    //   $(".a-button-selected .a-color-base:first"),
+    //   $("#priceblock_dealprice_row:first")
+    // );
 
-    const originalPrice = extractPrice(
-      $("#priceblock_ourprice:first"),
-      $(".a-price.a-text-price span.a-offscreen:first"),
-      $("#listPrice:first"),
-      $("#priceblock_dealprice:first"),
-      $(".a-size-base.a-color-price:first")
-    );
+    // const originalPrice = extractPrice(
+    //   $("#priceblock_ourprice:first"),
+    //   $(".a-price.a-text-price span.a-offscreen:first"),
+    //   $("#listPrice:first"),
+    //   $("#priceblock_dealprice:first"),
+    //   $(".a-size-base.a-color-price:first")
+    // );
 
-    const currency = extractCurrency($(".priceToPay span.a-price-symbol"));
-    const discount = extractDiscount(
-      $(
-        ".savingPriceOverride.reinventPriceSavingsPercentageMargin.savingsPercentage:first"
-      )
-    );
+    // const currency = extractCurrency($(".priceToPay span.a-price-symbol"));
+    // const discount = extractDiscount(
+    //   $(
+    //     ".savingPriceOverride.reinventPriceSavingsPercentageMargin.savingsPercentage:first"
+    //   )
+    // );
     // const data = {
     //   url,
     //   title,
